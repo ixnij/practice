735c54ea61f2bea58d1a4a1094905de5dd3342ae
Thomas Wu <ixnij.wu@outlook.com>
2021-07-21 18:59:59 +0800

    update, add a file finder written in python

--- python3/file_finder.py
+++ python3/file_finder.py
@@ -2,15 +2,18 @@
 
 import os
 
-def fdr(start):
+tot = 0 # setup a counter to let me know recurise funcall times. [Debug] 
+
+def fdr(start, kw):
     try:
         pwd = os.path.abspath(start) 
         for i in os.listdir(start):
             i = os.path.join(start, i)
             if os.path.isdir(i):
-                fdr(i)
-            print(i)
+                fdr(i, kw)
+                tot++
+            elif os.path.split(i)[1] == kw:
+                print(i)
     except Exception as ecp:
         print("Error")
         raise ecp
-
