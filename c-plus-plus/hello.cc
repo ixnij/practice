505e0bf16fc998d090ce7a16b8028282eda0b596
Thomas Wu <ixnij.wu@outlook.com>
2021-07-22 22:02:10 +0800

    simply commit

---
+++ c-plus-plus/hello.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main(void) {
+    std::cout << "Hello, World!" 
+        << std::endl
+        << "From CXX"
+        << std::endl
+        << R"+(
+        The RAW STRING!
+        )+" // do not forget CXX11!
+        << std::endl; 
+}
