3b339d5d4a0d4989a1f006a1cf133845b9ea7c88
Thomas Wu <ixnij.wu@outlook.com>
2021-07-25 16:25:28 +0800

    rewrite vector dot cc

--- c-plus-plus/STL/vector.cc
+++ c-plus-plus/STL/vector.cc
@@ -4,6 +4,16 @@
 using namespace std;
 
 int main([[maybe_unused]] int argc, [[maybe_unused]] char* const argv[]) {
+    vector<int> vtr {1, 2, 3, 4};
 
+    for ( auto itr = vtr.begin(); itr != vtr.end(); ++ itr ) {
+        cout << *itr << endl;
+        itr += itr;
+    }
 
+    cout << "== Re ==" << endl;
+
+    for ( auto ritr = vtr.rbegin(); ritr != vtr.rend(); ++ ritr ) {
+        cout << *ritr << endl;
+    }
 }
