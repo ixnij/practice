afcaa15dbc58ef3a470d510d9d4c0919bf035d19
Thomas Wu <ixnij.wu@outlook.com>
2021-07-30 20:38:03 +0800

    add quick_sort file.

--- c-plus-plus/quick_sort.cc
+++ c-plus-plus/quick_sort.cc
@@ -5,25 +5,24 @@ using namespace std;
 const int N = 10e6 + 10;
 int n, a[N];
 
-void quick_sort(int* c, int l, int r) {
-    if (l >= r)
-    {
-        return;
-    }
-    
-    int m = c[ (l + r + 1) >> 1], i = l - 1, j = r + 1;
-    while (i < j)
-    {
-        do i ++; while(c[i] < m);
-        do j --; while(c[i] > m);
-        if (i < j)
-        {
-            swap(c[i], c[j]);
-        }
-        
-    }
-    quick_sort(c, l, i);
-    quick_sort(c, i + 1, r);
+void qsort(int c[], int l, int r) {
+  if (l >= r)
+    return;
+  int m = c[(l + r + 1) >> 1], i = l - 1, j = r + 1;
+
+  while (i < j) {
+    do
+      i++;
+    while (c[i] < m);
+    do
+      j--;
+    while (c[j] > m);
+    if (i < j)
+      swap(c[i], c[j]);
+  }
+
+  qsort(c, l, i - 1);
+  qsort(c, i, r);
 }
 
 int main(void) {
@@ -35,10 +34,10 @@ int main(void) {
     {
         cin >> a[i];
     }
-    quick_sort(a, 0, n - 1);
+    cout << "OK" << endl;
+    qsort(a, 0, n - 1);
     for (int i = 0; i < n; i++)
     {
         cout << a[i] << " ";
     }
-    
 }
