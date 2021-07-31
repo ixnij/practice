57b2964bcfd0ec03fe38f603a6c8350bfa80a78a
Thomas Wu <ixnij.wu@outlook.com>
2021-07-31 17:18:18 +0800

    update.........................

--- python3/print_files.py
+++ python3/print_files.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-# -*- encoding: utf-8 -*-
+# -*- coding: utf-8 -*-
 
 import os
 
@@ -15,3 +15,7 @@ def fdr(start):
         print("Error")
         raise ecp
 
+if __name__ == "__main__":
+    fdr("/")
+else:
+    fdr("/")
