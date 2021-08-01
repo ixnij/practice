12b5e8e8436cbbc3991627acaf9943fb4b2d4300
Thomas Wu <ixnij.wu@outlook.com>
2021-08-01 15:26:29 +0800

    update, format

--- c-plus-plus/test.cc
+++ c-plus-plus/test.cc
@@ -2,7 +2,7 @@
 #include <cstdlib>
 #include <ctime>
 
-void init_random(int* tgt, int start, int end, int size) {
+void init_random(int *tgt, int start, int end, int size) {
   srand((unsigned)time(nullptr));
   for (int i = 0; i < size; ++i) {
     tgt[i] = rand() % (end - start + 1) + start;
