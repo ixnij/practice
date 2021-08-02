77a29a42c65dd6cc10de7c0df3c0d5dbff1b8cec
Thomas Wu <ixnij.wu@outlook.com>
2021-08-02 12:12:13 +0800

    Add a file.

---
+++ c-plus-plus/test_for_test.cc
@@ -0,0 +1,12 @@
+#include "test.h"
+#include <iostream>
+
+using namespace std;    
+
+const int N = 10e5;
+
+int main() {
+  int ary[N];
+  init_random(ary, 10, 10000, N);
+  for (int i : ary) { cout << ary << endl; }
+}
