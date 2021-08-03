9cd26c6fcbc2ff7e6d6278a047a03d3c620013af
Thomas Wu <ixnij.wu@outlook.com>
2021-08-03 11:08:50 +0800

    update.s

--- c-plus-plus/qzh.cc
+++ c-plus-plus/qzh.cc
@@ -27,9 +27,10 @@ int main() {
     }
     for(int i = 1; i <= n; ++ i) {
         for (int j = 1; j <= m; ++ j) {
-            b[i][j] = b[i][j] - b[i - 1][j] - b[i][j - 1] + b[i - 1][j - 1];
+            // b[i][j] = b[i][j] - b[i - 1][j] - b[i][j - 1] + b[i - 1][j - 1];
+             b[i][j] += b[i - 1][j] + b[i][j - 1] - b[i - 1][j - 1];
             // s[x2, y2] - S[x1 - 1, y2] - S[x2, y1 - 1] + S[x1 - 1, y1 - 1]
-            printf("%d", b[i][j]);
+            printf("%d ", b[i][j]);
         }
         printf("\n");
     }
