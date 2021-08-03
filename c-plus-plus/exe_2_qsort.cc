3ed2add82652fb4e8e049a3a2b7dcab2612324e9
Thomas Wu <ixnij.wu@outlook.com>
2021-08-03 11:02:15 +0800

    update....

--- c-plus-plus/exe_2_qsort.cc
+++ c-plus-plus/exe_2_qsort.cc
@@ -2,17 +2,18 @@
 
 using namespace std;
 
-const int N = 10e6 + 10;
+const int N = 10e5 + 10;
 
 void qsort(int* a, int l, int r) {
+    if(l >= r) return; 
     int mid = a[(l + r + 1) >> 1], i = l - 1, j = r + 1;
-    while(i != j) {
+    while(i < j) {
         do i++; while(a[i] < mid);
         do j--; while(a[j] > mid);
         if (i < j) swap(a[i], a[j]);
     }
-    qsort(a, l, n - 1);
-    qsort(a, n, r);
+    qsort(a, l, j - 1);
+    qsort(a, j, r);
 }
 
 int main(void) {
---
+++ c-plus-plus/qzh.cc
@@ -0,0 +1,36 @@
+#include <iostream>
+
+const int N = 1e3 + 10;
+
+using namespace std;
+int m, n, q, a[N][N], b[N][N];
+
+void insert(int x1, int y1, int x2, int y2, int c) {
+    b[x1][y1] += c;
+    b[x1][y2+1] -= c;
+    b[x2+1][y1] -= c;
+    b[x2+1][y2+1] += c;
+}
+
+int main() {
+    scanf("%d%d%d", &n, &m, &q);
+    for(int i = 1; i <= n; ++ i) {
+        for (int j = 1; j <= m; ++ j) {
+            scanf("%d", &a[i][j]);
+            insert(i, j, i, j, a[i][j]);
+        }
+    }
+    while(q --) {
+        int x1, y1, x2, y2, c;
+        scanf("%d%d%d%d%d", &x1, &y1, &x2, &y2, &c);
+        insert(x1, y1, x2, y2, c);
+    }
+    for(int i = 1; i <= n; ++ i) {
+        for (int j = 1; j <= m; ++ j) {
+            b[i][j] = b[i][j] - b[i - 1][j] - b[i][j - 1] + b[i - 1][j - 1];
+            // s[x2, y2] - S[x1 - 1, y2] - S[x2, y1 - 1] + S[x1 - 1, y1 - 1]
+            printf("%d", b[i][j]);
+        }
+        printf("\n");
+    }
+}
