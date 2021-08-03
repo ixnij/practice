34dfd603c7ce13174d6fa39fb394fbdab4d0ae04
Thomas Wu <ixnij.wu@outlook.com>
2021-08-03 12:40:27 +0800

    update.,...

---
+++ c-plus-plus/new_al_two.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <map>
+
+using namespace std;
+const int N = 1e5 + 10;
+int n;
+int a[N];
+
+int main() {
+        map<int, int> s;
+        cin >> n;
+        for(int i = 0; i < n; ++ i) {
+            cin >> a[i];
+        }
+        int res = 0;
+        for(int i = 0, j = 0; i < n; ++ i) {
+            s[a[i]] ++;
+            while(s[a[i]] > 1) {
+                s[a[j]] --;
+                j ++;
+            }
+           res = max(res, i - j + 1); 
+        }
+        cout << res << endl;
+}
