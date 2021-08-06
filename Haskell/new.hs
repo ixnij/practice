2a7a673693be091c615758c7994ae33852ab69b4
Thomas Wu <ixnij.wu@outlook.com>
2021-08-06 14:34:49 +0800

    update, for haskell

---
+++ Haskell/new.hs
@@ -0,0 +1,7 @@
+module Main where
+
+main :: IO ()
+
+main = do putStrLn "Hello. This is the first time in haskell."
+          putStrLn "But as long as we can finish the first program, we can done."
+          
