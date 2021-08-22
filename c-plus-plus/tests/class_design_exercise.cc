b50989ae9f5f9a597d7b21488eb023e181fd4f52
Thomas Wu <ixnij.wu@outlook.com>
2021-08-22 10:29:09 +0800

    update, for more.

---
+++ c-plus-plus/tests/class_design_exercise.cc
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <ostream>
+#include <string>
+
+using std::cout;
+
+class my_class {
+private:
+    std::string name;
+    std::string sex;
+    bool mir;
+    public:
+
+    my_class& operator << (std::ostream& os, )
+}
+
+    int main(int argc, char *argv[])
+    {
+
+        return 0;
+    }
