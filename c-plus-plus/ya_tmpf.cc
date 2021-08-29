6858f84cf25556aed98113e369f119c281394f17
Thomas Wu <ixnij.wu@gmail.com>
2021-08-29 17:55:42 +0800

    Update.

--- c-plus-plus/ya_tmpf.cc
+++ c-plus-plus/ya_tmpf.cc
@@ -1,14 +1,9 @@
-const int N = 1e6 + 10;
-int m;
-int l[N], r[N], e[N], idx;
+#include <bits/stdc++.h>
 
-inline void init() { r[0] = 1, l[1] = 0, idx = 2; }
+const int N = 1e5 + 10;
 
-inline void add(int k, int x) {
-    e[idx] = x, r[idx] = r[k], l[idx] = k;
-    l[r[k]] = idx, r[k] = idx, idx++;
+using namespace std;
+
+int main() {
+	
 }
-
-inline void rm(int x) { r[l[x]] = r[x], l[r[x]] = l[x]; }
-
-int main(void) {}
