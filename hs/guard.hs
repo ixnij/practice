e86778b7e01e7b8d6567f11505173e4cd592b4b6
Thomas Wu <ixnij.wu@gmail.com>
2021-09-20 21:04:36 +0800

    Update: using `where`

--- hs/guard.hs
+++ hs/guard.hs
@@ -1,6 +1,7 @@
 fn :: (RealFloat n) => n -> n -> String
 fn w h
-        | w / h ^ 2 <= 18.5 = "EMo"
-        | w / h ^ 2 <= 25.0 = "Pfft"
-        | w / h ^ 2 <= 30.0 = "FAT"
+        | bi <= 18.5 = "EMo"
+        | bi <= 25.0 = "Pfft"
+        | bi <= 30.0 = "FAT"
         | otherwise = "No prob"
+        where bi = w / h ^ 2
