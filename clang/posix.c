080b671a0b78fc7a5b2fdd5f1289f33be5a26eb7
Thomas Wu <ixnij.wu@gmail.com>
2021-10-01 18:10:12 +0800

    [clang]: review

---
+++ clang/posix.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void get_sytem_info();
+void command_args(int args, const char *argv[], void (*fn)());
+
+int main(int argc, const char *argv[]) {
+    command_args(argc, argv, get_sytem_info);
+    return 0;
+}
+
+void get_sytem_info() {
+    char *HOME = getenv("HOME");
+    char *PATH = getenv("PATH");
+    char *SHELL = getenv("SHELL");
+    printf("--- BEGIN OF ENVIRONMENT VARIABLES ---\n\n");
+    printf("Your HOME is: %s\nYour PATH: %s\nYour SHELL: %s\n\n", HOME, PATH,
+	   SHELL);
+    printf("--- END OF ENVIRONMENT VARIABLES ---\n");
+}
+
+void command_args(int args, const char *argv[], void (*fn)()) {
+    if (args == 1)
+	exit(1);
+    fn();
+}
+
+// Local Variables:
+// compile-command: "clang -o a.out posix.c"
+// End:
