eb6676e6b00c082b3958760589adbfca492fa31f
Thomas Wu <ixnij.wu@gmail.com>
2021-10-03 11:21:15 +0800

    [general]: update.

--- clang/hello.c
+++ clang/hello.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
+#include <math.h>
 
 int main(void) {
-    puts("Hello, World!");
+    fprintf(stdout, "Hello, World!\n");
+    double a = acos(20.1);
+    fprintf(stderr, "%f\n", a);
     return 0;
 }
+
+// Local Variables:
+// compile-command: "clang -o a.out hello.c"
+// End:
