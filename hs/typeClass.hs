65ec5333da100f00865cf048bc9c16edf2e87c25
Thomas Wu <ixnij.wu@gmail.com>
2021-10-05 10:09:45 +0800

    [haskell]: update.

--- hs/typeClass.hs
+++ hs/typeClass.hs
@@ -2,6 +2,12 @@ data LongTypeClassName = LongTypeClassName String deriving (Show, Eq, Ord)
 data Z = Z String Int deriving (Show, Eq, Ord)
 
 data Re = Re { name:: String
-             , hook :: String
+             , hook :: String -- The name of "::" is "Paamayim Nekudotayim".
              , isPerson :: Bool
              } deriving (Show)
+
+type P = Z
+
+data Ya a = Ya a deriving (Show)
+
+type F = Ya Int
