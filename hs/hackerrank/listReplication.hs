dd8d51c8e263a5868066e26e4a7811c476ca09e7
Thomas Wu <ixnij.wu@gmail.com>
2021-12-28 21:52:27 +0800

    [haskell] hackerrank: initial commit

---
+++ hs/hackerrank/listReplication.hs
@@ -0,0 +1,8 @@
+f :: Int -> [Int] -> [Int]
+f n = foldl (++) [] . map (\x -> [x|_<-[1..n]])
+
+-- This part handles the Input and Output and can be used as it is. Do not modify this part.
+
+main :: IO ()
+main = getContents >>=
+       mapM_ print. (\(n:arr) -> f n arr). map read. words
