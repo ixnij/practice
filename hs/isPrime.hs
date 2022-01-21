2004a6e2e17b19629580fe9aab4da3348e35b2e3
Thomas Wu <ixnij.wu@gmail.com>
2022-01-21 16:44:48 +0800

    [haskell] update

--- hs/isPrime.hs
+++ hs/isPrime.hs
@@ -6,6 +6,20 @@ isPrime x
     | x == 2    = True
     | otherwise = foldl' (\a b -> a && (x `mod` b /= 0)) True [2..(x-1)]
 
+pr :: IO Bool
+pr x y = do
+  putStrLn x
+  return y
+
+{- This function is not complete yet. TODO -}
+isPrime' x
+    | x < 2     = False
+    | x == 2    = True
+    | otherwise = foldl' m True [2..(x-1)]
+    where m a b = do
+            if x `mod` b == 0 then
+              pr (show x ++ " : " ++ show b) -- I can not go on..
+
 primes = filterPrime [2..]
 filterPrime :: [Integer] -> [Integer]
 filterPrime (p:xs) =
