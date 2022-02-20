2c656f5f9dc90e86a239c2af35d7536f91a34988
Thomas Wu <ixnij.wu@gmail.com>
2022-02-20 11:37:13 +0800

    [Haskell] update for "Fixed" function
    Add a version that contains a predicate

--- hs/Fix.hs
+++ hs/Fix.hs
@@ -13,3 +13,8 @@ fix'' :: (Eq b) => (a -> b -> b) -> a -> b -> b
 fix'' f x y
   | f x y == y = y
   | otherwise  = fix'' f x (f x y)
+
+fixp :: (a -> a -> Bool) -> (a -> a) -> a
+fixp c f x | c x (f x) = x
+           | otherwise = fixp c f (f x)
+           -- the arg "C" is a function, or a predicate
