abbc5dc574d48c37f2a582ad8c12a4e1787f43fb
Thomas Wu <ixnij.wu@gmail.com>
2022-02-25 21:45:13 +0800

    [haskell] impls (>>) in HS
    (>>) in F# is a function

---
+++ hs/comp.hs
@@ -0,0 +1,11 @@
+-- 我自己是觉得 haskell 提供的函数组合是比较自然的，即从从右边到左边的运算顺序。
+
+import Prelude hiding (
+                      (.))
+
+(.) :: (d -> b) -> (c -> d) -> (c -> b)
+f . g = \x -> f (g x)
+
+infixl 9 >>
+(>>) :: (a -> b) -> (b -> c) -> (a -> c)
+f >> g = \x -> g (f x)
