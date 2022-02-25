18ef02522eebc68e74276414581d7c53f393a0bd
Thomas Wu <ixnij.wu@gmail.com>
2022-02-25 21:00:22 +0800

    [haskell] update

--- hs/range.hs
+++ hs/range.hs
@@ -3,7 +3,7 @@ module Main where
 main :: IO ()
 
 vars   = [x | x <- [1 .. 20] , x `mod` 2 == 0]
--- Attention: x `mod` 2 == 
+-- Attention: x `mod` 2 ==
 -- equals to even x
 vartwo = [ x + y | x <- [1 .. 200], y <- [201 .. 400]]
 
