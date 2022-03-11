18a5bd627a7d07905895378fc26724153fde2784
Thomas Wu <ixnij.wu@gmail.com>
2022-03-11 18:00:34 +0800

    [haskell] just update

---
+++ hs/newType.hs
@@ -0,0 +1,6 @@
+{-# LANGUAGE GeneralisedNewtypeDeriving #-}
+
+newtype MySimpleType = ST Integer deriving (Num, Eq, Ord)
+instance Show MySimpleType where
+  show (ST n) = "This is " ++ show n ++ "."
+type Stype = Integer
