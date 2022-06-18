598094c3863b27aa6cc65ff1c208a0a3d7008ce2
Thomas Wu <ixnij.wu@gmail.com>
2022-06-18 21:19:57 +0800

    [haskell] Simple ZIPPER

---
+++ hs/Zypper.hs
@@ -0,0 +1,15 @@
+-- Zypper is openSUSE's package manager, it's SIMPLE ZIPPER exactly
+
+data Zypper a = Zypper [a] a [a] deriving Show
+
+fromList :: [a] -> Zypper a
+fromList (x:xs) = Zypper [] x xs
+fromList _ = error "Too few fields in the List"
+
+next :: Zypper n -> Zypper n
+next (Zypper a b (c:cs)) = Zypper (b:a) c cs
+next single = single
+
+prev :: Zypper n -> Zypper n
+prev (Zypper (a:as) b c) = Zypper as a (b:c)
+prev single = single
