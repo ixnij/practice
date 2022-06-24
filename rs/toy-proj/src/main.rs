5943e387609ac52bd7d8841f0e05d89affbdcdeb
Thomas Wu <ixnij.wu@gmail.com>
2022-06-24 15:26:49 +0800

    [rust] update main.rs

--- rs/toy-proj/src/main.rs
+++ rs/toy-proj/src/main.rs
@@ -16,8 +16,20 @@ fn main() {
     println!("{}", n);
     n = "Worl";
     println!("{}", n);
+    let n1: &str = &my_str[..3];
+    let ns: &str = &my_str[5..];
+    println!("There are two silces: {} and {}", n1, ns);
+    let _len = my_str.len();
+    assert_eq!(24, _len);
+    let (_, y) = returns_itself_and_its_length(String::from("r"));
+    println!("{}", y);
 }
 
 fn add_something_to_my_string(sth: &str, will_be_changes: &mut String) -> () {
     will_be_changes.push_str(sth);
+}
+
+fn returns_itself_and_its_length(s: String) -> (String, usize) {
+    let p = s.len();
+    (s, p)
 }
